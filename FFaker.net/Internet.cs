013534da6d9ba85d7e8a4a47b94ec5852c8f90a8
Yan Justino <yanlimaj@gmail.com>
2014-07-01 11:41:57 -0300

    travis file

---
+++ FFaker.net/Internet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFaker.Base;
+
+namespace NFaker
+{
+  public static class Internet
+  {
+    private static string HOSTS = "gmail.com yahoo.com hotmail.com";
+    private static string DISPOSABLE_HOSTS = "mailinator.com suremail.info spamherelots.com binkmail.com safetymail.info";
+    private static string DOMAIN_SUFFIXES = "co.uk com us ca biz info name";
+    private static string SLUG_DELIMITERS = "- _ .";
+  }
+}
