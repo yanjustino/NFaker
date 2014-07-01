43e2a9f8d21e4501c7d320859528b187edc4c037
Yan Justino <yanlimaj@gmail.com>
2014-07-01 10:58:18 -0300

    Add Geolocation

---
+++ FFaker.net.tests/GeolocationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+namespace NFaker.Tests
+{
+  [TestClass]
+  public class GeolocationTests
+  {
+    [TestMethod]
+    [Description("When call Geolocation.Latitude returns a double velue")]
+    public void Geolocation_Latitude()
+    {
+      var latitude = NFaker.Geolocation.Latitude.ToString();
+      Assert.IsTrue(Regex.Match(latitude, "[0-9]").Success);
+      Console.Write(latitude);
+    }
+
+    [TestMethod]
+    [Description("When call Geolocation.Longitude returns a double velue")]
+    public void Geolocation_Longitude()
+    {
+      var longitude = NFaker.Geolocation.Longitude.ToString();
+      Assert.IsTrue(Regex.Match(longitude, "[0-9]").Success);
+      Console.Write(longitude);
+    }
+  }
+}
